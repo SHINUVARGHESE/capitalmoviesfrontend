262e09f4c02bc554f1ad375d5d977f78660b55cf
SHINUVARHSESE <shinushinu01997@gmail.com>
2021-11-03 19:03:54 +0530

    url updated

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,2 +1,2 @@
-export const serverurl = 'http://localhost:3001'
+export const serverurl = 'https://capitalmoviesserver.herokuapp.com'
 export const imageurl = 'https://image.tmdb.org/t/p/original' 
