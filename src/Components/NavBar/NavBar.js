63c431c59f0f062e109942841ec02248b5a5648a
SHINUVARHSESE <shinushinu01997@gmail.com>
2021-11-06 00:45:14 +0530

    wish list added and favorate list delete icon fixed

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -38,17 +38,35 @@ function NavBar() {
         document.body.classList.remove('active-modal')
     }
 
+   const deleteFavorate=(movieId)=>{
+       const ids= {
+           movieId,
+           userId:user.id
+       }
+  var r = window.confirm("Are you want to delet the favorate movie ?");
+  if (r == true) {
+    axios.post(`${serverurl}/deletefavorate`,ids).then((data)=>{
+       if(data.data.status){
+           alert(" Movie deleted from favorate")
+       }
+    })
+  } else {
+    console.log("canceled");
+  }
+      
+    }
+
     return (
         <div className='navbar'>
             <h1 className="logo"><Link to="/">Capital Movies</Link></h1>
             <div style={{ marginLeft: "35%", color: "white" }}>{user.username &&
                 <div className="dropdown">
-                    <button className="dropbtn"><h3>Favorate</h3></button> 
+                    <button className="dropbtn"><h3>Favorates</h3></button> 
                     <div className="dropdown-content">
                         {
                            favoratemovies ? favoratemovies.map((item,ky) => {
                                 return (
-                                    <a >{item.moviedata.title}</a>
+                                    <a  style={{color:"white"},{hover:{color:"black"}}}>{item.moviedata.title}<button onClick={()=>{deleteFavorate(item.moviedata.id)}} style={{float:"right", backgroundColor:"black", border:"none",padding:"5px", color:"red",fontWeight:"bold",fontSize:"13px",cursor:"pointer"}}>x</button></a>
                                 )
                             }) : <a>no movies</a>
                         }
